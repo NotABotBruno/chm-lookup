83ca9ebecd8b36ea5fe695ef8ba4a64178c5e68b
Bruno Souza <brunoanevan_msouza@hotmail.com>
2024-07-20 21:55:28 -0300

    Add powershell command

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+import { exec } from 'child_process'
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -31,15 +32,28 @@ function activate(context) {
 			const selection = editor.selection;
 			const selectedText = editor.document.getText(selection);
 
-			const config = vscode.workspace.getConfiguration('chmFunctionFinder');
-			const chmFilePath = config.get('chmFilePath');
-
-			if (selectedText) {
-                vscode.window.showInformationMessage(`Searching CHM for: ${selectedText} in ${chmFilePath}`);
-                // Implement your logic to search the selected text in the CHM file
-            } else {
-                vscode.window.showInformationMessage('No text selected.');
-            }
+			if(selectedText){
+				const config = vscode.workspace.getConfiguration('chmFunctionFinder');
+				const chmFilePath = config.get('chmFilePath');
+				
+				if(chmFilePath){
+					const command = `powershell -Command "Get-Content '${chmFilePath}' | Select-String '${selectedText}'"`;
+	
+					exec(command, (error, stdout, stderr) => {
+						if (error) {
+							vscode.window.showErrorMessage(`Error: ${error.message}`);
+							return;
+						}
+						if (stderr) {
+							vscode.window.showErrorMessage(`Error: ${stderr}`);
+							return;
+						}
+						vscode.window.showInformationMessage(`Search Results:\n${stdout}`);
+					});
+				} else {
+					vscode.window.showInformationMessage('CHM file path is not set. Please configure it in the settings.');
+				}
+			}			
 		}
 	})
 	context.subscriptions.push(disposable);
